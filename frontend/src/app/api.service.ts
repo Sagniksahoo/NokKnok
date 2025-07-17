38bf211d2447572cd9b24ba55546763c64bf445f
Sagnik Sahoo <79495648+Sagniksahoo@users.noreply.github.com>
2025-07-17 18:58:41 +0530

    frontend setting

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() { }
+  
+  private baseUrl = "  http://127.0.0.1:5000/"
+
+
+}
