9be1a350bf543bc8e7a1ffe18de11b332e4034fb
Sagnik Sahoo <79495648+Sagniksahoo@users.noreply.github.com>
2025-07-17 20:11:54 +0530

    database setup

--- backend/index.py
+++ backend/index.py
@@ -1,8 +1,29 @@
 from flask import Flask, jsonify
 from flask_cors import CORS 
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+
+# Load .env
+load_dotenv()
+
+
 
 app = Flask(__name__)        
-CORS(app)                    
+CORS(app)                 
+
+
+# Connect to MongoDB
+mongo_uri = os.getenv("MONGO_URI")  # e.g. 'mongodb://localhost:27017/nokknok_db'
+client = MongoClient(mongo_uri)
+db = client.get_database()  # Gets 'nokknok_db'
+
+# Sample collection
+visitors = db.visitors
+
+
+
 
 @app.route('/')
 def home():
